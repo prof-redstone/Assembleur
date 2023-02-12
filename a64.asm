9e1248649f142772baf686cf4a575f2b7d3add26
Tom Demagny <tom.demagny@laposte.net>
2023-02-12 13:56:11 +0100

    test print name

--- a64.asm
+++ a64.asm
@@ -1,7 +1,5 @@
 bits 64
 ;vectorcall
-;%define hello 'Hello World !';macro qui estremplacé avant assemblage
-;%define message_length 13;macro de la longueur de la chaine
 
 extern GetStdHandle ; gestionnaire de péripherique
 extern WriteConsoleA ; ecriture en mode console
@@ -14,29 +12,56 @@ STD_INPUT_HANDLE: equ -10
 SHADOWSPACE_SIZE: equ 32+8
 
 section .data
-    message db "Bonjour tout le monde !", 10 ; 10 -> retour a la ligne
-    message_length equ $-message
-
+    message: db "Quel est ton nom ?"
+    MESSAGE_LENGTH: equ $-message
+    USERNAME_MAX_LENGTH: equ 12 ; 10 carac + 2 pour le retour à la ligne
 
 section .bss
-    written resq 1 ;reserve un qword *1
-    mydata: resb 64; octet*64
+    written: resd 1 
+    read: resd 1
+    username: resb USERNAME_MAX_LENGTH
 
 section .text
     global main
     main:
-        mov rcx, STD_OUTPUT_HANDLE ;premier argument -11 pour la sortie standar, 
-        call GetStdHandle
-
         sub rsp, SHADOWSPACE_SIZE
+        ;------------------------
+
+        ;demande de saisie
+        mov rcx, STD_OUTPUT_HANDLE
+        call GetStdHandle
 
         mov rcx, rax
         mov rdx, message
-        mov r8, message_length; longueur de la chaine 
+        mov r8, MESSAGE_LENGTH
         mov r9, written
-        mov qword [rsp+32], 0
+        mov qword [rsp + SHADOWSPACE_SIZE], 0
         call WriteConsoleA
 
+        ;lecture du clavier
+        mov rcx, STD_INPUT_HANDLE
+        call GetStdHandle
+
+        mov rcx, rax
+        mov rdx, username
+        mov r8, USERNAME_MAX_LENGTH
+        mov r9, read
+        mov qword [rsp + SHADOWSPACE_SIZE], 0
+        call ReadConsoleA
+
+
+        ;Affichage de la saisie
+        mov rcx, STD_OUTPUT_HANDLE
+        call GetStdHandle
+
+        mov rcx, rax
+        mov rdx, username
+        mov r8, USERNAME_MAX_LENGTH
+        mov r9, written
+        mov qword [rsp + SHADOWSPACE_SIZE], 0
+        call WriteConsoleA
+
+        ;------------------------
         add rsp, SHADOWSPACE_SIZE
         
         xor rcx, rcx
